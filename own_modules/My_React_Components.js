875f6c09a7ad199641488cdc642c8d5413043068
mathijs28498 <33691132+mathijs28498@users.noreply.github.com>
2020-05-28 17:26:58 +0200

    Added default colour properties MyServey

--- own_modules/My_React_Components.js
+++ own_modules/My_React_Components.js
@@ -2,19 +2,23 @@ import React from 'react'
 
 export class MySurvey extends React.Component {
     render() {
+        // Gets the default rgb values from user for color input
+        let rgb = [this.props.r, this.props.g, this.props.b];
+        let hex = rgbDecToHex(rgb);
+
         return (
             <form>
                 <p>This is a survey for {this.props.company}</p>
-                <label for="first_name">First name</label><br />
+                <label htmlFor="first_name">First name</label><br />
                 <input type="text" id="first_name" placeholder="First name here"></input>
                 <br />
                 <br />
-                <label for="last_name">Last name</label><br />
+                <label htmlFor="last_name">Last name</label><br />
                 <input type="text" id="last_name" placeholder="Last name here"></input>
                 <br />
                 <br />
-                <label for="fav_color">Favourite colour</label><br />
-                <input type="color" id="fav_color" name="favourite colour" value="#00ff00"></input>
+                <label htmlFor="fav_color">Favourite colour</label><br />
+                <input type="color" id="fav_color" name="favourite colour" value={hex}></input>
                 <br />
                 <br />
                 <input type="submit" value="Continue" />
@@ -22,3 +26,30 @@ export class MySurvey extends React.Component {
         );
     }
 };
+
+// Turns an array from decimal rgb values into a hexadecimal rgb string
+function rgbDecToHex(rgb) {
+    let res = '#';
+    for (let i = 0; i < 3; i++) {
+        let val = rgb[i];
+
+        // Check if the value is set and if it is a number
+        if (val === undefined || isNaN(val)) {
+            res += "00";
+        } else {
+            res += componentToHex(val);
+        }
+    }
+    return res;
+}
+
+// Turn single decimal number to hexadecimal number
+function componentToHex(c) {
+    let hex = parseInt(c, 10).toString(16);
+
+    // Handle small and too big numbers
+    if (hex.length > 2) hex = "00";
+    else if (hex.length === 1) hex = "0" + hex;
+    
+    return hex;
+}
--- stories/assignment.stories.js
+++ stories/assignment.stories.js
@@ -1,5 +1,4 @@
 import React from 'react'
-// import { Button } from '@storybook/react/demo';
 import { MySurvey } from '../own_modules/My_React_Components.js';
 import { Button } from '@storybook/react/demo';
 import { action } from '@storybook/addon-actions';
@@ -15,7 +14,7 @@ button.story = {
 }
 
 export const survey = () => (
-    <MySurvey company="Plek"></MySurvey>
+    <MySurvey company="Plek" r="231" g="115" b="52"></MySurvey>
 );
 survey.story = {
     name: 'My react component (form)',
